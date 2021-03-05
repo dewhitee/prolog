8cb5b99a0f32fd0318b9e2600c6a696d7e9bcbcc
DewhiteE <dewhiteeofficial@gmail.com>
2021-03-05 13:41:22 +0200

    tmp

--- monkey_n_banana.pl
+++ monkey_n_banana.pl
@@ -21,7 +21,27 @@ move(state(X1, on_floor, XC, Y), walk(X1, X2), state(X2, on_floor, XC, Y)).
 
 % win
 victory(state(_, _, _, has), []).
-% M and L are lists
-victory(S, [M | L]) :-
-    move(S, M, S2), % S2 state is that
-    victory(S2, L). % S2 state is victory
+% M and L are lists, M - transitions (currentMoves) list, L - nextMoves
+victory(CurrentState, [CurrentMoveDescription | NextMoves]) :-
+    move(CurrentState, CurrentMoveDescription, NextState), % S2 state is that
+    victory(NextState, NextMoves). % S2 state is victory
+
+
+% Add new element to linked-list end
+addHead(H, T, [H | T]).
+
+% Add multiple elements to linked-list start, backward recursive step
+concat([], L2, L2).
+concat([H | T], L2, [H | L3]) :-
+    concat(T, L2, L3).
+
+% Check if element is in list
+contains(X, [X, _]).
+contains(X, [_ | T]) :-
+    contains(X, T).
+
+% Show ways how we can delete certain X element in list
+showDelete(X, [X | T], T).
+showDelete(X, [H | T], [H | T2]) :-
+    showDelete(X, T, T2).
+
