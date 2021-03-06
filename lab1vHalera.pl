e12a1bd3fd671490d9d28b2db6fb55fc93ea9f6e
HaleraK <savkin80@gmail.com>
2021-03-06 20:27:02 +0200

    upd labHalerak

--- lab1vHalera.pl
+++ lab1vHalera.pl
@@ -27,14 +27,18 @@
 
 
 % При выполнении
-move(state(officeOfYellowPapper, noFilm, noCamera), go, state(garageOutCity, noFilm, noCamera)).
-move(state(garageOutCity, noFilm, noCamera), grab, state(garageOutCity, noFilm, camera)).
+move(state(officeOfYellowPapper, noFilm, noCamera), go1, state(garageOutCity, noFilm, noCamera)).
+move(state(garageOutCity, noFilm, noCamera), grab1, state(garageOutCity, noFilm, camera)).
 
-move(state(garageOutCity, noFilm, noCamera), go, state(dachaGeneral, noFilm, camera)).
-move(state(dachaGeneral, noFilm, camera), shootTheFilm, state(dachaGeneral, film, camera)).
+move(state(garageOutCity, noFilm, noCamera), go2_1, state(dachaGeneral, noFilm, camera)).
+move(state(dachaGeneral, noFilm, camera), shootTheFilm1_1, state(dachaGeneral, film, camera)).
 
-move(state(garageOutCity, noFilm, noCamera), go, state(officeCompany, noFilm, camera)).
-move(state(officeCompany, noFilm, camera), shootTheFilm, state(officeCompany, film, camera)).
+move(state(garageOutCity, noFilm, noCamera), go2_2, state(officeCompany, noFilm, camera)).
+move(state(officeCompany, noFilm, camera), shootTheFilm1_1, state(officeCompany, film, camera)).
+
+move(state(X1, A, B), go(X1, X2), state(X2, A, B)).
+move(state(X, A1, B), grab(A1, A2), state(X, A2, B)).
+move(state(X, A, B1), shootTheFilm(B1, B2), state(X, A, B1)).
 
 victory(state(dachaGeneral, film, camera), []).
 victory(state(officeCompany, film, camera), []).
